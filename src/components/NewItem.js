20d0af1150b59a890390c641a2756528620f42dd
Natalie Miller <nataliemiller03@gmail.com>
2018-11-28 12:13:35 -0500

    Component state demonstration

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -7,7 +7,8 @@ class NewItem extends Component {
   state = { value: '' };
 
   handleChange = event => {
-    // Do something when the state of this input changes.
+    const value = event.target.value;
+    this.setState({ value });
   };
 
   handleSubmit = event => {
@@ -16,9 +17,9 @@ class NewItem extends Component {
 
     event.preventDefault();
 
-    // Do something when a new value is submitted.
+    onSubmit({value, id: uniqueId, packed: false});
 
-    // Reset the state of the component.
+    this.setState({ value: '' });
   };
 
   render() {
